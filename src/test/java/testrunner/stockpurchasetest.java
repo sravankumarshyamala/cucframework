35b826a77f4f214619c5d5d522198d4d26cf99df
sravankumar shyamala <sravankumar shyamala@LAPTOP-4NL42U64>
2025-03-04 12:59:57 +0530

    commit

---
+++ src/test/java/testrunner/stockpurchasetest.java
@@ -0,0 +1,23 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+//import org.junit.runner.RunWith;
+
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Feature/Purchase.feature",
+                 glue="step",
+                 dryRun=false)
+
+public class stockpurchasetest
+{
+
+}
